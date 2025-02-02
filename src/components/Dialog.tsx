d33525c4cb36201d2a85a34a10fc967e14393dba
victor <vvergarar@bam.com.mx>
2025-02-01 23:18:52 -0600

    add: basic dialog info

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,6 +1,12 @@
-import DialogTitle from '@mui/material/DialogTitle';
-import Dialog from '@mui/material/Dialog';
 import { Pokemon } from '../utils/models';
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Dialog,
+  DialogTitle,
+  Typography,
+} from '@mui/material';
 interface DialogComponentProps {
   open: boolean;
   handleClose: () => void;
@@ -10,15 +16,35 @@ interface DialogComponentProps {
 export const DialogComponent: React.FC<DialogComponentProps> = ({
   open,
   handleClose,
+  pokemon,
 }) => {
   return (
-    <Dialog
-      open={open}
-      onClose={() => {
-        handleClose();
-      }}
-    >
-      <DialogTitle>jajajaj</DialogTitle>
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle>{pokemon?.name}</DialogTitle>
+
+      <Card>
+        <CardMedia
+          component="img"
+          height="200"
+          image={pokemon?.image || '/api/placeholder/200/200'}
+          alt={pokemon?.name}
+          sx={{ objectFit: 'contain', p: 2 }}
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            Nombre: {pokemon?.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Id: {pokemon?.id}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Hp: {pokemon?.hp}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Ataque:{pokemon?.attack}
+          </Typography>
+        </CardContent>
+      </Card>
     </Dialog>
   );
 };
